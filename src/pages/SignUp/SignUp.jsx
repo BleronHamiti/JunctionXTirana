f0e65632ea708ff2f1e89e1eaedfd8c4979a4e5c
BleronHamiti <bleronhamiti1@gmail.com>
2024-04-22 01:54:36 +0200

    Initial commit

---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,11 @@
+// import Login2 from "../../components/pages/Login/Login";
+// function SignUp() {
+//     return(
+//        <>
+//         <Login2/>
+//         <Sig
+//        </>
+//     )
+// }
+
+// export default SignUP;
